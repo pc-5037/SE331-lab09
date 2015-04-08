5fa5482a57ec7f5bde6b9d8df81a2ca448c16a05
Chanthicha016 <se552115016@vr.camt.info>
2015-04-08 11:16:32 +0700

    2files w/o pp

--- src/main/java/camt/se331/shoppingcart/config/AppConfig.java
+++ src/main/java/camt/se331/shoppingcart/config/AppConfig.java
@@ -14,6 +14,7 @@ import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
 import java.util.Locale;
 
+@Import({PersistenceContext.class})
 @EnableWebMvc
 @Configuration 
 @ComponentScan(basePackages = {"camt.se331.shoppingcart"})
